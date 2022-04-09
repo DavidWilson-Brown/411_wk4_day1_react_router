893b3ac212796d93e5ccd2583071a7f6ed6d5bcd
David Wilson-Brown <wilsonbrowndavid@gmail.com>
2022-04-08 22:14:07 -0500

    Final updates to project

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import cars from '../cars.json'
+import React from 'react';
+import cars from '../cars.json';
 // import material ui components here //
-// Container, Paper, Chip //
+import { Container } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Chip } from '@material-ui/core';
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const id = props.match.params.id;
+  const carFound = cars.find((car) => car.id == id);
+  return (
+    <Container maxWidth="sm" className="car-container">
+      <Paper className="car-paper">
+        <h2>{carFound.Name}</h2>
+        {Object.entries(carFound).map(([key, value]) => {
+            return <Chip label={`${key}: ${value}`} />
+        })}
+      </Paper>
+    </Container>
+  );
+};
 
-export default Car
+export default Car;
